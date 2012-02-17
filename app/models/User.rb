8fa6b4f96eeb1c86099243aa06f9b5fffbc96700
Matt Farmer <matt.farmer@openstudy.com>
2012-02-16 23:32:23 -0500

    Implement first iteration of devise-powered User model.

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,5 +1,17 @@
 class User
   include MongoMapper::Document
   
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :validatable
+  
   key :username, String
+  key :email, String
+  key :encrypted_password, String
+  key :password_salt, String
+  key :admin, Boolean, :default => false
+  key :editor, Boolean, :default => false
+  key :active, Boolean, :default => true
+  timestamps!
 end
