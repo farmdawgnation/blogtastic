80dc9981f06faa9c0a3d6346f57b86e324626411
Matt Farmer <matt.farmer@openstudy.com>
2012-02-17 23:31:07 -0500

    Draft functionality for admin/pages controller.

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,22 +1,47 @@
 class Admin::PagesController < AdminApplicationController
+  respond_to :html, :json
+  
   def index
+    @pages = Page.all
   end
   
-  def show
+  def show(id)
+    @page = Page.find(id)
   end
 
   def new
+    @page = Page.new
   end
 
   def create
+    @page = Page.new
+    
+    @page.update_attributes params[:page]
+    @page.save
+    
+    respond_to do |format|
+      format.html { redirect_to admin_page(@page.id) }
+      format.json { render :json => @page._json }
+    end
   end
 
-  def edit
+  def edit(id)
+    @page = Page.find(id)
   end
 
-  def update
+  def update(id)
+    @page = Page.find(id)
+    
+    @page.update_attributes params[:page]
+    
+    respond_to do |format|
+      format.html { redirect_to admin_page(@page.id) }
+      format.json { render :json = @page._json }
+    end
   end
 
-  def destroy
+  def destroy(id)
+    @page = Page.find(id)
+    @page.destroy
   end
 end
