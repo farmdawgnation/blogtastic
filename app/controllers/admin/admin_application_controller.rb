6f46cd38ab4d5694d77b96dcc6e9790b666a1ba0
Matt Farmer <matt.farmer@openstudy.com>
2012-02-22 23:42:23 -0500

    Properly set up AdminApplicationController and declare default layout.

--- app/controllers/admin/admin_application_controller.rb
+++ app/controllers/admin/admin_application_controller.rb
@@ -3,8 +3,10 @@
 # suite. If you're looking to change how things behave on the
 # frontend, see app/controllers/application_controller.rb instead.
 ###
-class AdminApplicationController < ActionController::Base
+class Admin::AdminApplicationController < ActionController::Base
   protect_from_forgery
   
   before_filter :authenticate_user!
+  
+  layout "admin/application"
 end
