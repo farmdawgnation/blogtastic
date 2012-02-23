c93989e8023a4930db8f8d9e608f1d8ea8595610
Matt Farmer <matt.farmer@openstudy.com>
2012-02-22 23:45:10 -0500

    Flesh out pages administration controller.

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,12 +1,14 @@
-class Admin::PagesController < AdminApplicationController
+class Admin::PagesController < Admin::AdminApplicationController
   respond_to :html, :json
   
   def index
     @pages = Page.all
   end
   
-  def show(id)
-    @page = Page.find(id)
+  def show
+    @page = Page.find(params[:id])
+    
+    if @page.nil? then page_not_found_error end
   end
 
   def new
@@ -20,28 +22,48 @@ class Admin::PagesController < AdminApplicationController
     @page.save
     
     respond_to do |format|
-      format.html { redirect_to admin_page(@page.id) }
+      format.html { redirect_to admin_pages_path }
       format.json { render :json => @page._json }
     end
   end
 
-  def edit(id)
-    @page = Page.find(id)
+  def edit
+    @page = Page.find(params[:id])
+    
+    if @page.nil? then page_not_found_error end
   end
 
-  def update(id)
-    @page = Page.find(id)
+  def update
+    @page = Page.find(params[:id])
     
     @page.update_attributes params[:page]
     
     respond_to do |format|
       format.html { redirect_to admin_page(@page.id) }
-      format.json { render :json = @page._json }
+      format.json { render :json => @page._json }
     end
   end
 
-  def destroy(id)
-    @page = Page.find(id)
-    @page.destroy
+  def destroy
+    @page = Page.find(params[:id])
+    
+    unless @page.nil?
+      @page.destroy
+      
+      respond_to do |format|
+        format.html { redirect_to admin_pages_path, :notice => "The page has been deleted." }
+        format.json { head :ok }
+      end
+    else
+      page_not_found_error
+    end
   end
+  
+  private
+    def page_not_found_error
+      respond_to do |format|
+        format.html { redirect_to admin_pages_path, :alert => "That page was not found." }
+        format.json { head :not_found }
+      end
+    end
 end
